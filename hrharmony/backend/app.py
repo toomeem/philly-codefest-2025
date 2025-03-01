56a44915525ffc82b5cc3bee514dbd235f72b126
Evan Toomey <etoomey81@gmail.com>
2025-03-01 15:58:44 -0500

    trying to fix dependencies

--- hrharmony/backend/app.py
+++ hrharmony/backend/app.py
@@ -1,8 +1,5 @@
-import json
-import os
 import random
 import string
-import sys
 
 import boto3
 from flask import Flask, request
@@ -10,28 +7,32 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-def generate_password():
-  characters = string.ascii_letters + string.digits + string.punctuation
-  password = "".join(random.choice(characters) for _ in range(12))
-  return password
+# def generate_password():
+#   characters = string.ascii_letters + string.digits + string.punctuation
+#   password = "".join(random.choice(characters) for _ in range(12))
+#   return password
 
-@app.route("/upload_file", methods=["POST"])
-def upload_file():
-  file = request.files["file"]
+@app.route("/", methods=["GET"])
+def home():
+  return "HR Harmony"
 
-  file.save(f"uploads/{file.filename}")
-  return "200"
+# @app.route("/upload_file", methods=["POST"])
+# def upload_file():
+#   file = request.files["file"]
+
+#   file.save(f"uploads/{file.filename}")
+#   return "200"
 
 
-@app.route("/user", methods=["POST"])
-def create_user():
-  request_data = request.get_json()
-  email = request_data["email"]
-  department = request_data["department"]
-  first_name = request_data["first_name"]
-  last_name = request_data["last_name"]
-  password = generate_password()
-  return "200"
+# @app.route("/user", methods=["POST"])
+# def create_user():
+#   request_data = request.get_json()
+#   email = request_data["email"]
+#   department = request_data["department"]
+#   first_name = request_data["first_name"]
+#   last_name = request_data["last_name"]
+#   password = generate_password()
+#   return "200"
 
 
 
