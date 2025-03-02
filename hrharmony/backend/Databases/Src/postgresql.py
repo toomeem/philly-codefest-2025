aea382b870011407477a7df66c57929ab371f3b5
Evan Toomey <etoomey81@gmail.com>
2025-03-01 19:44:26 -0500

    made CRUD funcs for user db

--- hrharmony/backend/Databases/Src/postgresql.py
+++ hrharmony/backend/Databases/Src/postgresql.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 import psycopg2
 from dotenv import load_dotenv
@@ -7,6 +8,7 @@ load_dotenv()
 
 
 def create_user(email, department, first_name, last_name, password):
+  id = str(uuid.uuid4())
   conn = psycopg2.connect(
     host=os.getenv("RDS_ENDPOINT"),
     database="postgres",
@@ -15,17 +17,13 @@ def create_user(email, department, first_name, last_name, password):
     port="5432"
   )
   cur = conn.cursor()
-  print(email, department, first_name, last_name, password)
-  cur.execute(f'''INSERT INTO users (email, department, first_name, last_name, password) VALUES
-    ('{email}', '{department}', '{first_name}', '{last_name}', '{password}')
-  ''')
-
+  cur.execute(f'''INSERT INTO users (id, email, department, first_name, last_name, password) VALUES
+  ('{id}', '{email}', '{department}', '{first_name}', '{last_name}', '{password}')''')
   conn.commit()
-
   cur.close()
   conn.close()
 
-def fetch_user(email, password):
+def fetch_user(id=None, email=None, password=None):
   conn = psycopg2.connect(
     host=os.getenv("RDS_ENDPOINT"),
     database="postgres",
@@ -34,8 +32,73 @@ def fetch_user(email, password):
     port="5432"
   )
   cur = conn.cursor()
-  cur.execute(f"SELECT * FROM users WHERE email='{email}' AND password='{password}'")
+  if id:
+    cur.execute(f"SELECT * FROM users WHERE id='{id}'")
+  elif email and password:
+    cur.execute(f"SELECT * FROM users WHERE email='{email}' AND password='{password}'")
+  else:
+    return
   user = cur.fetchone()
   cur.close()
   conn.close()
   return user
+
+def update_user(id, email=None, department=None, first_name=None, last_name=None, password=None):
+  user = fetch_user(id=id)
+  if not user:
+    return
+  conn = psycopg2.connect(
+    host=os.getenv("RDS_ENDPOINT"),
+    database="postgres",
+    user=os.getenv("RDS_USERNAME"),
+    password=os.getenv("RDS_PASSWORD"),
+    port="5432"
+  )
+  cur = conn.cursor()
+  if email:
+    cur.execute(f"UPDATE users SET email='{email}' WHERE id='{id}'")
+  if department:
+    cur.execute(f"UPDATE users SET department='{department}' WHERE id='{id}'")
+  if first_name:
+    cur.execute(f"UPDATE users SET first_name='{first_name}' WHERE id='{id}'")
+  if last_name:
+    cur.execute(f"UPDATE users SET last_name='{last_name}' WHERE id='{id}'")
+  if password:
+    cur.execute(f"UPDATE users SET password='{password}' WHERE id='{id}'")
+  conn.commit()
+  cur.close()
+
+def drop_db():
+  conn = psycopg2.connect(
+    host=os.getenv("RDS_ENDPOINT"),
+    database="postgres",
+    user=os.getenv("RDS_USERNAME"),
+    password=os.getenv("RDS_PASSWORD"),
+    port="5432"
+  )
+  cur = conn.cursor()
+  cur.execute("DROP TABLE users")
+  conn.commit()
+  cur.close()
+  conn.close()
+
+def create_db():
+  conn = psycopg2.connect(
+    host=os.getenv("RDS_ENDPOINT"),
+    database="postgres",
+    user=os.getenv("RDS_USERNAME"),
+    password=os.getenv("RDS_PASSWORD"),
+    port="5432"
+  )
+  cur = conn.cursor()
+  cur.execute('''CREATE TABLE IF NOT EXISTS users (
+    id VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    department VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+  )''')
+  conn.commit()
+  cur.close()
+  conn.close()
